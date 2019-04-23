a8084becf187b29c9ed4ef07b49e322ff11916de
nevton <nevton>
2019-04-23 17:16:53 -0300

    lol

--- assets/asteroids-base.py
+++ assets/asteroids-base.py
@@ -20,6 +20,26 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+class Player(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        
+        pygame.sprite.Sprite.__init__(self)
+        
+        player_img = pygame.image.load(path.join(img_dir, "playerShip1_orange.png"))
+        self.image = player_img
+        
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        
+        self.image.set_colorkey(BLACK)
+        
+        self.rect = self.image.get_rect()
+        
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        
+        
+
 # Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
@@ -36,6 +56,11 @@ clock = pygame.time.Clock()
 # Carrega o fundo do jogo
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
+
+player = Player()
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 # Comando para evitar travamentos.
 try:
@@ -57,6 +82,7 @@ try:
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
+        all_sprites.draw(screen)
         
         # Depois de desenhar tudo, inverte o display.
         pygame.display.flip()
